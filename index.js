d879e4ee2bca0e4e86a66a30477efef54383c984
bahnisikha <bahnisikhadhar@gmail.com>
2023-01-27 13:31:22 +0530

    layout of chessboard

---
+++ index.js
@@ -0,0 +1,8 @@
+const chessBox = document.querySelectorAll(".chess_box");
+
+chessBox.forEach((box)=>{
+    // box.addEventListener("click",(e)=>{
+    //     console.log(e.target.innerText);
+    // })
+   
+})
