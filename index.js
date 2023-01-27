87565531819a5b90dfe7cc67e8521812dcf31fb0
bahnisikha <bahnisikhadhar@gmail.com>
2023-01-27 22:19:16 +0530

    added layout using css and js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const chessBox = document.querySelectorAll(".chess_box");
+// const turnShow = document.getElementById("tog");
 
-// chessBox.forEach((image)=>{
-    // image.addEventListener("click",(e)=>{
-    //     console.log(image.innerText);
-    // })
-// })
+
+// //**************************************ADDING IMAGES HERE****************************************************/
 
 function imageAdd() {
 
@@ -12,16 +10,40 @@ function imageAdd() {
 
         if (image.innerText.length !== 0) {
             if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
-                image.innerHTML = `<img class='allpawn' src="./Image/${image.innerText}.png" alt="">`
+                image.innerHTML = `<img class='allpawn' src="${image.innerText}.png" alt="">`
 
             }
 
             else {
-               image.innerHTML = `<img src="./Image/${image.innerText}.png" alt="">`
+               image.innerHTML = `<img src="${image.innerText}.png" alt="">`
             }
         }
     })
 }
 
 imageAdd();
+
+// //******************************************ADDING COLOUR TO THE BOXES HERE*************************************/
+
+function colorBackground() {
+
+    chessBox.forEach(color => {
+       
+        getId = color.id; //"b801"
+        arr = getId.split("");// ["b","8","0","1"]
+        arr.shift(); // ["8","0","1"]
+        aside = Number(arr.pop()) // 1
+        aup = Number(arr.shift())// 8
+        a = aside + aup
+        console.log(Number(a));
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(241, 215, 182)'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = 'rgb(104, 62, 7)'
+        }
+
+    })
+}
+colorBackground();
 
