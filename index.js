7fb820214e7f2bf42f34c3a84b303dc29c5c2bb9
bahnisikha <bahnisikhadhar@gmail.com>
2023-01-27 16:39:09 +0530

    added images and color

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const chessBox = document.querySelectorAll(".chess_box");
 
-chessBox.forEach((image)=>{
+// chessBox.forEach((image)=>{
     // image.addEventListener("click",(e)=>{
     //     console.log(image.innerText);
     // })
-    
-    
-    
-})
+// })
+
+function imageAdd() {
+
+    chessBox.forEach(image => {
+
+        if (image.innerText.length !== 0) {
+            if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+                image.innerHTML = `<img class='allpawn' src="./Image/${image.innerText}.png" alt="">`
+
+            }
+
+            else {
+               image.innerHTML = `<img src="./Image/${image.innerText}.png" alt="">`
+            }
+        }
+    })
+}
+
+imageAdd();
+
